216836cf1a0257a5cfbc504b2353c4c9768cff9d
skyryo <ryosakaiwmagptja@gmail.com>
2023-10-17 00:45:12 +0900

    get dyanmic routing params

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,8 @@
-export default function Post() {
-	return <div>記事詳細ページ</div>
+export default function Post({ params }: { params: { id: string } }) {
+	return (
+		<>
+			<div>記事詳細ページ</div>
+			<p>選択した記事は{params.id}です</p>
+		</>
+	)
 }
