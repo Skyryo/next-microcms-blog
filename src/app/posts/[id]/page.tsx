3b35f5709b20ca3e0d80caf555d88f8572604e73
skyryo <ryosakaiwmagptja@gmail.com>
2023-10-17 23:41:23 +0900

    add error bundling

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,13 +2,17 @@ import { getPost } from '@/app/libs/microcms'
 import parse from 'html-react-parser'
 
 export default async function Post({ params }: { params: { id: string } }) {
-	const { title, content } = await fetchPost(params.id)
-	return (
-		<>
-			<h1>タイトル：{title}</h1>
-			<div>{parse(content)}</div>
-		</>
-	)
+	try {
+		const { title, content } = await fetchPost(params.id)
+		return (
+			<>
+				<h1>タイトル：{title}</h1>
+				<div>{parse(content)}</div>
+			</>
+		)
+	} catch (error) {
+		throw error
+	}
 }
 
 //fetch post data
