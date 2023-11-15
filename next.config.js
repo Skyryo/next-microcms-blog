cc94288a6c01b0445058f1e315e6ced6a970751a
skyryo <ryosakaiwmagptja@gmail.com>
2023-11-16 01:55:57 +0900

    add pwa configure

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+	dest: 'public',
+})
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.microcms-assets.io',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.microcms-assets.io',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
+})
